8521dcd4a4d0b00ebd092cfc14e5404361a182a3
anreetka <anreetka@sheridancollege.ca>
2024-08-21 09:24:33 -0400

    adding original project files

---
+++ DisconnectedModelExample/Data.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DisconnectedModelExample
+{
+    class Data
+    {
+        SqlConnection conn;
+        SqlDataAdapter adapter;
+        DataSet ds;
+        DataTable dataTable;
+        SqlCommandBuilder cmdBuilder;
+
+        string connStr = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=NorthWind;Integrated Security=True";
+
+        public string ConnStr { get { return connStr; } }
+
+        public Data()
+        {
+            conn = new SqlConnection(ConnStr);
+            string query = "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM Products";
+            adapter = new SqlDataAdapter(query, conn);
+            cmdBuilder = new SqlCommandBuilder(adapter);
+            FillDataSet();
+        }
+
+        public void FillDataSet()
+        {
+            ds = new DataSet();
+            adapter.Fill(ds);
+            dataTable= ds.Tables[0];
+
+            //define primary key for data table because data set cannot see columns so it doesn't give us a primary key
+            DataColumn[] pk = new DataColumn[1];
+            pk[0] = dataTable.Columns["ProductID"];
+            pk[0].AutoIncrement = true; //set autoincrement to true so that it can increase automatically
+
+            dataTable.PrimaryKey = pk;  //set the primary key equal to pl
+        }
+
+        public DataTable LoadDataTable()
+        {
+            FillDataSet();
+            return dataTable;
+        }
+
+        public DataRow FindProductById(int id)
+        {
+            DataRow row = null;
+            row = dataTable.Rows.Find(id);
+            return row;
+
+        }
+
+        public DataTable InsertProduct(String name, decimal price, short units)
+        {
+            DataRow row = dataTable.NewRow();  //new empty row created
+            row["ProductName"] = name;  //add values to new Row 
+            row["UnitPrice"] = price;
+            row["UnitsInStock"] = units;
+
+            dataTable.Rows.Add(row);   //add row to the Product Table
+ 
+
+            adapter.InsertCommand = cmdBuilder.GetInsertCommand();  //insert query
+            adapter.Update(dataTable); //update original database using Update
+            return dataTable;
+        }
+
+        public DataTable DeleteProductById(int id)
+        {
+            DataRow productRow = FindProductById(id);
+            if(productRow != null)
+            {
+                productRow.Delete();
+                MessageBox.Show("Row deleted!");
+            }
+            else
+            {
+                MessageBox.Show("id doesn't exist");
+            }
+
+            //you can only modify the data you added due to dependencies with other tables
+            adapter.DeleteCommand = cmdBuilder.GetDeleteCommand();
+            adapter.Update(dataTable);
+            return dataTable;
+        }
+    }
+}
---
+++ DisconnectedModelExample/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+
+namespace DisconnectedModelExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Data data;
+        public MainWindow()
+        {
+            InitializeComponent();
+            data = new Data();
+        }
+
+        private void btnLoadAllProducts_Click(object sender, RoutedEventArgs e)
+        {
+            DataTable tblProduct = data.LoadDataTable();
+            grdProducts.ItemsSource = tblProduct.DefaultView;
+        }
+
+        private void btnFind_Click(object sender, RoutedEventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            DataRow row = data.FindProductById(id);  //will return data row
+            if(row!=null)
+            {
+                txtName.Text = row["ProductName"].ToString();
+                txtPrice.Text = row["UnitPrice"].ToString();
+                txtQuantity.Text = row["UnitsInStock"].ToString();
+            }
+            else
+            {
+                MessageBox.Show("Row not found!");
+            }
+           
+        }
+
+        private void btnInsert_Click(object sender, RoutedEventArgs e)
+        {
+            string name = txtName.Text;
+            decimal price = decimal.Parse(txtPrice.Text);
+            short units = short.Parse(txtQuantity.Text);
+
+            DataTable productTable = data.InsertProduct(name, price, units);
+
+            grdProducts.ItemsSource = productTable.DefaultView;
+            
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            DataTable productTable = data.DeleteProductById(id);
+            grdProducts.ItemsSource = productTable.DefaultView;
+        }
+
+        private void btnShowWindow2_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 win2 = new Window2();
+            win2.Show();
+            
+        }
+    }
+}
